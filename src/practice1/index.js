1295578e7cded7222782dcd9e90ac4d4ddf51851
linlifu2 <linlin.fu@thoughtworks.com>
2020-07-03 21:16:34 +0800

    finish all practice

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,7 @@
+import * as aws from "./constant";
+import getURL from "./utils";
 
+const url = getURL(aws.ADDRESS, aws.PATH, aws.PORT);
+const region = aws.REGION;
+
+export { url, region };
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,20 @@
-export const getPoetry
+function handleResponse(response) {
+  return response.json().then((json) => {
+    if (response.ok) {
+      const { content, origin, author } = json;
+      return [origin, author, content];
+    }
+    return Promise.reject({
+      ...json,
+      status: response.status,
+      statusText: response.statusText,
+    });
+  });
+}
+
+export const getPoetry = async () => {
+  const result = await fetch("https://v1.jinrishici.com/all.json")
+    .then(handleResponse)
+    .catch((error) => console.log(error));
+  return result;
+};
